6b041353d440d3f33513b676d2d5df127f4b7cf9
allenpbiju <allenpbiju1999@gmail.com>
2020-09-12 22:18:31 +0530

    Added static folder and landing page

--- FC/First_commit/views.py
+++ FC/First_commit/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request,"landing.html")
 
 def create(request):
 	return render(request,"create.html")
